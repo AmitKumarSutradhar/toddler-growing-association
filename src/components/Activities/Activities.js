5b87c56013eaf4bf7a7d858ecc295ab12a037c34
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-09-30 00:30:06 +0600

    Activities Component Added

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,20 +1,20 @@
-// import React from 'react';
-// import './Activities.css'
+import React from 'react';
+import './Activities.css'
 
-// const Activities = ({ activity }) => {
+const Activities = ({ activity }) => {
 
-//     const { name, img, desc, age, time } = activity;
-//     console.log(name);
+    const { name, img, desc, age, time } = activity;
+    console.log(name);
 
-//     return (
-//         <div>
-//             <img src={img} alt=""></img>
-//             <p>{name}</p>
-//             <p>{desc}</p>
-//             <p>{age}</p>
-//             <p>{time}</p>
-//         </div>
-//     );
-// };
+    return (
+        <div>
+            <img src={img} alt=""></img>
+            <p>{name}</p>
+            <p>{desc}</p>
+            <p>{age}</p>
+            <p>{time}</p>
+        </div>
+    );
+};
 
-// export default Activities;
+export default Activities;
